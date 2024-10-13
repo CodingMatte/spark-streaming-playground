9d48f9fd487ce50133f946e9d71452ee6fbfdc7e
[matteo-incicco] <matteoincicco@gmail.com>
2024-10-13 16:29:15 +0100

    added Schemas, readFromFiles, demoTriggers and data in resources

--- src/main/java/structuredstreaming/StreamingDataFrames.java
+++ src/main/java/structuredstreaming/StreamingDataFrames.java
@@ -4,10 +4,12 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.streaming.StreamingQuery;
+
+import static common.Schemas.stocksSchema;
 import static org.apache.spark.sql.functions.*;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkContext;
+import org.apache.spark.sql.streaming.Trigger;
 
 public class StreamingDataFrames {
 
@@ -52,9 +54,52 @@ public class StreamingDataFrames {
     query.awaitTermination();
   }
 
+  public static void readFromFiles() throws Exception {
+    Dataset<Row> stocksDF =
+        spark
+            .readStream()
+            .format("csv")
+            .option("header", "false")
+            .option("dateFormat", "MMM d yyyy")
+            .schema(stocksSchema)
+            .load("src/main/resources/data/stocks");
+
+    stocksDF.writeStream().format("console").outputMode("append").start().awaitTermination();
+  }
+
+  public static void demoTriggers() throws Exception {
+    Dataset<Row> lines =
+        spark
+            .readStream()
+            .format("socket")
+            .option("host", "localhost")
+            .option("port", 12345)
+            .load();
+
+    // Write the stream to the console
+    lines
+        .writeStream()
+        .format("console")
+        .outputMode("append")
+        .trigger(
+            // Trigger.ProcessingTime("5 seconds") // every 5 seconds run the query
+            // Trigger.Once() // single batch, then terminate
+            Trigger.Continuous(
+                "2 seconds") // experimental, every 2 sec create a batch with whatever you have
+            )
+        .start()
+        .awaitTermination();
+  }
+
   // Main method
+  // Before starting the demo, start the Netcat server doing:
+  // nc -lk 12345
   public static void main(String[] args) throws Exception {
+    // Set log level to ERROR - avoid INFO messages every second
     spark.sparkContext().setLogLevel("ERROR");
-    readFromSocket();
+
+    // readFromSocket();
+    // readFromFiles();
+    demoTriggers();
   }
 }
